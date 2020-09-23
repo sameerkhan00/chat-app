19c8f719280ec27f18f99ce0368aaf63df417c50
sameerkhan00 <sk2655391@gmail.com>
2020-09-23 15:25:02 +0500

    chat app

--- app.js
+++ app.js
@@ -1,79 +1,82 @@
-let signup =()=>{
-    window.location="sigup.html"
+//   var myName=prompt("enter ur name");
+var list = document.getElementById("list");
+//  let realTime =()=>{
+
+//  firebase.database().ref("message").on("child_added",function(data){
+
+//   });
+
+//}
+
+let sendMessage = () => {
+  var input = document.getElementById("text");
+  var key = firebase.database().ref("message").push().key;
+  var todo = {
+    message: input.value,
+    key: key,
+  };
+  firebase.database().ref("message").set(todo);
+};
+
+let signPage = () => {
+  window.location = "sign.html";
+};
+
+let signInWith = () => {
+  let email = document.getElementById("email");
+  let password = document.getElementById("password");
+  console.log(email.value)
+  console.log(password)
+  
+//   firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+// .then((result)=>{
+    
+//     console.log(result)
+// })
+// .catch(function(error) {
+//     // Handle Errors here.
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//     console.log(errorMessage)
+//     // ...
+//   });
+
 }
 
 
-let signUp=()=>{
-    var email=document.getElementById("mail");
-    var password=document.getElementById("pass")
-   
+let login = () => {
+  var email = document.getElementById("mail");
+  var pass = document.getElementById("pass");
+  console.log(email.value);
+  console.log(pass.value);
 
-    firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
-    .then((result)=>{
-        console.log(result)
-        // window.location="chat.html"
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email.value, pass.value)
+    .then((result) => {
+      window.location = "chat.html";
 
+      //    console.log(result)
+      //    console.log(email.value)
     })
-            .catch(function(error) {{
-                alert(error)     }
-    
-        // ...
-      });
-
-
-}
-
-
-let log =()=>{
-    var email=document.getElementById("loginMail");
-    var password=document.getElementById("loginPass")
-    
-
-    firebase.auth().signInWithEmailAndPassword(email.value, password.value)
-    .then((result)=>{
-         window.location="chat.html"
-        
-    //    console.log(result) 
-    //    console.log(email.value)
-    })
-    .catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        alert(errorMessage)
-        // ...
-      });
-      
-    }
-    
-    
-var sendMessage =()=>{
-    var input=document.getElementById("message");
-    firebase.database().ref("message").push().set({
-        //  send:mail.value,
-        msg:input,
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert(errorMessage);
+      // ...
     });
-}
+};
 
-
-  //   var myName=prompt("enter ur name");
-  var list =document.getElementById("list");
-firebase.database().ref("message").on("child_added",function(data){
-    var li=document.createElement("li");
-    var liText=document.createTextNode(data.val().value);
-     li.appendChild(liText)
-    list.appendChild(li)
-   
- })
-
-
-let signOut =()=>{
-    firebase.auth().signOut()
-    .then(function() {
-        window.location="index.html"
-        // Sign-out successful.
-      }).catch(function(error) {
-        // An error happened.
-      });
-}
-
+let signOut = () => {
+  firebase
+    .auth()
+    .signOut()
+    .then(function () {
+      window.location = "index.html";
+      // Sign-out successful.
+    })
+    .catch(function (error) {
+      // An error happened.
+    });
+};
